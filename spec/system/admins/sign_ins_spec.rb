7d53f3428b052e735f6061c74337c72fcb1d2ec8
桐山恵輔 <kfp.a@icloud.com>
2025-09-09 10:25:05 +0900

    toastの修正

--- spec/system/admins/sign_ins_spec.rb
+++ spec/system/admins/sign_ins_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "管理者ログイン", type: :system do
+RSpec.describe '管理者ログイン', type: :system do
   it '管理者は管理画面にログインできる' do
     create(:admin, email: 'alice@example.com', password: 'password12345')
 
