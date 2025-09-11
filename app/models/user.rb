589d73869519fd594c0b8466fe83023778042f7b
桐山恵輔 <kfp.a@icloud.com>
2025-09-11 23:54:09 +0900

    プロフィール画像の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  has_one_attached :profile_image
+
   validates :name, presence: true
+  validates :profile_image, blob: {
+    content_type: %w[image/png image/jpg image/jpeg],
+    size_range: 1..(5.megabytes)
+  }
 end
