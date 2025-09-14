ecdf090af6eb75e184239a4e14fd9e5b3b47f9c7
桐山恵輔 <kfp.a@icloud.com>
2025-09-15 04:02:57 +0900

    編集ページの追加

--- app/controllers/users/families_controller.rb
+++ app/controllers/users/families_controller.rb
@@ -1,6 +1,11 @@
 class Users::FamiliesController < Users::ApplicationController
+  before_action :set_family, only: %i[edit update]
+
   def new
     @family = Family.new
+  end
+
+  def edit
   end
 
   def create
@@ -14,15 +19,22 @@ class Users::FamiliesController < Users::ApplicationController
     end
   end
 
-  def edit
-  end
-
   def update
+    if @family.update(family_params)
+      redirect_to profile_path(current_user), notice: '家族情報を更新しました。'
+    else
+      flash.now[:alert] = '家族情報を更新できませんでした。'
+      render :edit, status: :unprocessable_content
+    end
   end
 
   private
 
   def family_params
     params.expect(family: %i[name])
+  end
+
+  def set_family
+    @family = current_user.family
   end
 end
