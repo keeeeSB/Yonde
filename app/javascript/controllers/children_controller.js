81a5cc393d3413cbb610b63e8d23b8ab108f3f2c
桐山恵輔 <kfp.a@icloud.com>
2025-09-19 04:48:11 +0900

    お子様フォームの追加

---
+++ app/javascript/controllers/children_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="children"
+export default class extends Controller {
+  static targets = ["container", "template"]
+
+  add(event) {
+    event.preventDefault()
+    const content = this.element.dataset.template
+    const newId = new Date().getTime()
+    const newContent = content.replace(/NEW_RECORD/g, newId)
+    this.containerTarget.insertAdjacentHTML("beforeend", newContent)
+  }
+
+  remove(event) {
+    event.preventDefault()
+    const field = event.target.closest(".nested-fields")
+    if (field) field.remove()
+  }
+}
