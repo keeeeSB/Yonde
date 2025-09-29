9ea5b8b3e702173d593fb5a8c16c5b33fb4d134f
桐山恵輔 <kfp.a@icloud.com>
2025-09-30 01:19:02 +0900

    LibraryBooksコントローラーにアクションを追加

---
+++ app/controllers/family/library/library_books_controller.rb
@@ -0,0 +1,17 @@
+class Family::Library::LibraryBooksController < Family::ApplicationController
+  before_action :set_book, only: %i[show destroy]
+
+  def show
+  end
+
+  def destroy
+    @book.destroy!
+    redirect_to family_library_path(@family), notice: '本棚から削除しました。', status: :see_other
+  end
+
+  private
+
+  def set_book
+    @book = @family.library.books.find(params[:id])
+  end
+end
