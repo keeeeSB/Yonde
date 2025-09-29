fbab8105cc46f61fed31d0152a88a1e9c5ee7663
桐山恵輔 <kfp.a@icloud.com>
2025-09-29 09:17:52 +0900

    booksテーブルのカラムの修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,11 +2,7 @@ class Book < ApplicationRecord
   has_many :libraries, dependent: :destroy
 
   validates :title, presence: true
-  validates :published_date, presence: true
-  validates :description, presence: true
-  validates :isbn, presence: true
-  validates :page_count, presence: true
-  validates :image_url, presence: true
+  validates :systemid, presence: true, uniqueness: true
 
   scope :default_order, -> { order(created_at: :asc, id: :asc) }
 end
