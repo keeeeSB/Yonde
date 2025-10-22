3aa7d863dff4e84239df001e72805ff97beeca1c
KeeeeSB <kfp.a@icloud.com>
2025-10-22 12:26:58 +0900

    軽微な修正

--- app/javascript/controllers/comment_toggle_controller.js
+++ app/javascript/controllers/comment_toggle_controller.js
@@ -5,13 +5,13 @@ export default class extends Controller {
     static targets = ['display', 'form', 'button']
 
   connect() {
-      this.formTarget.classList.add("d-none")
+      this.formTarget.classList.add('d-none')
   }
 
     toggleForm() {
-        this.displayTarget.classList.toggle("d-none")
-        this.formTarget.classList.toggle("d-none")
+        this.displayTarget.classList.toggle('d-none')
+        this.formTarget.classList.toggle('d-none')
         this.buttonTarget.textContent =
-            this.formTarget.classList.contains("d-none") ? "編集" : "閉じる"
+            this.formTarget.classList.contains('d-none') ? '編集' : '閉じる'
     }
 }
