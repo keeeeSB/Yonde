a99c3bf66caae0549e22cf1ccbf07c91e6238ad6
KeeeeSB <kfp.a@icloud.com>
2025-10-30 03:50:59 +0900

    いいね機能の追加

---
+++ app/controllers/families/libraries/books/reading_logs/likes_controller.rb
@@ -0,0 +1,23 @@
+class Families::Libraries::Books::ReadingLogs::LikesController < Families::Libraries::Books::ApplicationController
+  before_action :set_like, only: %i[destroy]
+
+  def create
+    @like = current_user.likes.build(reading_log: @reading_log)
+    if @like.save
+      redirect_back fallback_location: root_path, notice: 'いいねをしました。'
+    else
+      redirect_back fallback_location: root_path, alert: 'いいねできませんでした。'
+    end
+  end
+
+  def destroy
+    @like.destroy!
+    redirect_back fallback_location: root_path, notice: 'いいねを取り消しました。', status: :see_other
+  end
+
+  private
+
+  def set_like
+    @like = current_user.likes.find(params[:id])
+  end
+end
